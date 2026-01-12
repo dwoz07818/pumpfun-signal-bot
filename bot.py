501b1585310d4e7c84f4865c68890be9cf660e32
dwoz07818 <dwoz07818@gmail.com>
2026-01-12 07:30:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,29 @@ import time
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
-def send(msg):
-    bot.send_message(chat_id=CHAT_ID, text=msg)
+def send_signal(coin):
+    msg = f"""
+🚀 EARLY SIGNAL
+
+Name: {coin['name']}
+Liquidity: ${coin['liquidity']}
+Dev Hold: {coin['dev_hold']}%
+Top10: {coin['top10']}%
+Holders: {coin['holders']}
+Score: {coin['score']}
+"""
+
+    if "image" in coin and coin["image"]:
+        bot.send_photo(
+            chat_id=CHAT_ID,
+            photo=coin["image"],
+            caption=msg
+        )
+    else:
+        bot.send_message(chat_id=CHAT_ID, text=msg)
 
 while True:
-    signals = scan_new_coins()
-    for s in signals:
-        send(s)
+    coins = scan_new_coins()
+    for c in coins:
+        send_signal(c)
     time.sleep(60)

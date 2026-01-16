feb2049e0dc99e26174f56b74a133865ccff2f35
dwoz07818 <dwoz07818@gmail.com>
2026-01-16 07:34:21 +0530

    Delete x_hype.py

--- x_hype.py
+++
@@ -1,4 +0,0 @@
-def check_x_hype(name):
-    # TEST logic: keyword based (X hype compulsory later)
-    keywords = ["AI", "MEME", "COIN"]
-    return any(k.lower() in name.lower() for k in keywords)
